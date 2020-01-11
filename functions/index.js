04345f7eee1e32d5ff4cb4ddeae3056d55492273
james947 <jmuriuki947@gmail.com>
2020-01-11 23:52:53 +0300

    Update onFile change function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,34 @@
 const functions = require('firebase-functions');
+const gcs = require('@google-cloud/storage')
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.onFileChange = functions.storage.object().onFinalize( request => {
- console.log(event)
- return;
+ const object = event.stopImmediatePropagation;
+ const bucket = object.bucket;
+ const contentType = object.contentType;
+ const filePath = object.name;
+ console.log('File change detected function execution started')
+
+ if (path.basename(filePath).startsWith('renamed-')){
+     console,log('we already renamed the file')
+     return;
+ }
+
+ const destBucket= gcs.bucket(bucket);
+ const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+ const metadata = {contentType: contentType };
+
+ return destBucket.file(filePath).download({
+     destination: tmpFilePath,
+ }).then(() => {
+    return destBucket.upload(tmpFilePath, {
+        destination: 'renamed-' + path.basename(filePath),
+        metadata: metadata
+    })
+ })
 });
 
 
